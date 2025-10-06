9c5a4c9a67bdbfe569aa1ba0647a18d9b100abcb
Manuela0613 <martinezgilmanuela@gmail.com>
2025-10-06 15:25:53 -0500

    commit inicial

--- Niveles y dificultad.py
+++ Niveles y dificultad.py
@@ -1,3 +1,66 @@
+class PalabraJuego:
+    def __init__(self, texto: str, usada: bool) -> None:
+        self.texto: str = texto
+        self.precision: float = 0.0
+        self.errores: int = 0
+        self.usada: bool = usada
+
+    def calcular_errores(self, entrada: str) -> int:
+        errores = 0
+        i = 0
+
+        while i < len(self.texto) and i < len(entrada):
+            if self.texto[i] != entrada[i]:
+                errores += 1
+            i = i + 1
+
+        if len(self.texto) > len(entrada):
+            errores = errores + (len(self.texto) - len(entrada))
+        elif len(entrada) > len(self.texto):
+            errores = errores + (len(entrada) - len(self.texto))
+
+        self.errores = errores
+        return errores
+
+    def comparar_con(self, palabra_correcta: str) -> None:
+        errores = self.calcular_errores(palabra_correcta)
+        total = len(palabra_correcta)
+
+        if total > 0:
+            aciertos = total - errores
+            self.precision = aciertos / total
+        else:
+            self.precision = 0.0
+
+    def es_correcta(self, palabra_correcta: str) -> bool:
+        if self.texto.lower().strip() == palabra_correcta.lower().strip():
+            self.usada = True
+        else:
+            self.usada = False
+
+        return self.usada
+
+    def __str__(self) -> str:
+        return f"Palabra escrita: {self.texto}, Errores: {self.errores}, Precisión: {round(self.precision * 100, 2)}%"
+# Palabra original del juego
+palabra_juego = "python"
+
+# Palabra escrita por el jugador
+jugador = PalabraJuego("pythno")
+
+# Comparar ambas
+jugador.comparar_con(palabra_juego)
+
+# Mostrar resultados
+jugador.mostrar_resultado()
+
+# Verificar si es exactamente igual
+print("¿Es correcta?:", jugador.es_correcta(palabra_juego))
+
+
+class RepositorioPalabras:
+    pass
+
 class Nivel:
     def __init__(self, numero: int, tiempo_limite: float, precision_requerida:float) -> None:
         self.numero: int = numero
@@ -7,6 +70,7 @@ class Nivel:
 
     def obtener_palabra(self):
         pass
+    
     def generar_palabra(self):
         pass
 
@@ -28,20 +92,5 @@ class Nivel:
     def reiniciar(self):
         pass
 
-class PalabraJuego:
-    def __init__(self, texto: str, dificultad: int, usada: bool) -> None:
-        self.texto: str = texto
-        self.dificultad: int = dificultad
-        self.usada: bool = usada
 
-    def comparar(self, entrada:str):
-        pass
-
-    def calcular_errores(self, entrada: str):
-        pass
-
-    def palabra_correcta(self, entrada: str):
-        pass
-
-    def es_correcta(self):
-        pass
+    
