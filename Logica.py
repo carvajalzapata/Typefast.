bfbdb046ff9141e197a34ef8ecb109047a28e7bd
carzap <carvajalzapatamateo@gmail.com>
2025-10-05 19:47:50 -0500

    metodo iniciar partida y verificar palabra

--- Logica.py
+++ Logica.py
@@ -1,3 +1,4 @@
+import time
 class Partida:
 
     def __init__(self, jugador: Jugador):
@@ -6,3 +7,13 @@ class Partida:
         self.historial_niveles = []
         self.estado = "inactiva"self.tiempo_inicio = None
 
+    def iniciar_partida(self):
+        print(f"¡Bienvenido {self.jugador.nombre}! Comenzando el nivel 1...")
+        self.estado = "activa"
+        self.tiempo_inicio = time.time()
+
+    def verificar_palabra(self, palabra_original: str, palabra_usuario: str):
+        palabra  = PalabraJuego(palabra_original)
+        errores = palabra.contar_errores(palabra_usuario)
+        precision = palabra.calcular_precision(palabra_usuario)
+        return errores, precision
