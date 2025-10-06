0391262d9d1f008cee3ce3eb47367b43d97ee897
Manuela0613 <martinezgilmanuela@gmail.com>
2025-10-05 20:08:52 -0500

    commit inicial

--- Niveles y dificultad.py
+++ Niveles y dificultad.py
@@ -5,10 +5,27 @@ class Nivel:
         self.precision_requerida: float = precision_requerida
         self.palabras: list = []
 
+    def obtener_palabra(self):
+        pass
+    def generar_palabra(self):
+        pass
+
     def puede_pasar(self, precision:float, velocidad:int):
         pass
 
     def siguiente_palabra(self):
+        pass
+
+    def reducir_tiempo(self):
+        pass
+
+    def pasar_nivel(self):
+        pass
+
+    def mostrar_informacion(self):
+        pass
+
+    def reiniciar(self):
         pass
 
 class PalabraJuego:
@@ -17,8 +34,14 @@ class PalabraJuego:
         self.dificultad: int = dificultad
         self.usada: bool = usada
 
+    def comparar(self, entrada:str):
+        pass
+
     def calcular_errores(self, entrada: str):
         pass
 
     def palabra_correcta(self, entrada: str):
+        pass
+
+    def es_correcta(self):
         pass
