88d4ce9ac8e9b7cef670ffae4575653c2d619b9a
carzap <carvajalzapatamateo@gmail.com>
2025-11-01 18:41:00 -0500

    iniciar y registrar

--- Logica.py
+++ Logica.py
@@ -1,3 +1,5 @@
+import time
+
 class Partida:
     def __init__(self, jugador: Jugador, repositorio: RepositorioPalabras):
         self.jugador = jugador
@@ -7,6 +9,31 @@ class Partida:
         self.puntaje_total = 0
         self.en_curso = False
         self.resultados = []
+
+    def iniciar(self):
+        print(f"\nIniciando partida para {self.jugador.nombre}...")
+        self.en_curso = True
+        self.nivel_actual.generar_palabra()
+        self.tiempo_inicio = time.time()
+
+    def registrar_entrada(self, entrada: str):
+        palabra = self.nivel_actual.obtener_palabra()
+        if not palabra:
+            print("No hay palabra actual disponible.")
+            return
+
+        precision = palabra.comparar_con(entrada)
+        print(palabra)
+
+        self.nivel_actual.siguiente_palabra()
+        return precision
+
+
+
+
+
+
+
 
 
 
